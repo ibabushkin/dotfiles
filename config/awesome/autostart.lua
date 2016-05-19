72e256a84f429428af719cecf9e3744cb2a32529
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-05-19 23:19:14 +0200

    further additions

--- config/awesome/autostart.lua
+++ config/awesome/autostart.lua
@@ -1,8 +1,8 @@
 local awful = require("awful")
 
 function logs ()
-    awful.util.spawn_with_shell("termite --class Log -e /home/thewormkill/.log2")
-    awful.util.spawn_with_shell("termite --class Log -e /home/thewormkill/.log1")
+    awful.util.spawn_with_shell("termite --class Log -e ~/.log2")
+    awful.util.spawn_with_shell("termite --class Log -e ~/.log1")
 end
 
 function monitors ()
