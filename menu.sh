822aa22bf53b397ae21de1b444717999b6cc3b9b
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-08-13 16:25:37 +0200

    Updated menu

--- menu.sh
+++ menu.sh
@@ -1,1 +1,4 @@
-sed 's/,.*//' menu | dmenu -i | xargs -I{} grep {} menu | sed 's/.*,//' | /bin/sh &
+# read in the menu file and use dmenu to run an entry from it
+sed 's/,.*//' menu | sort |
+dmenu -i | xargs -I{} grep {} menu |
+sed 's/.*,/exec /' | /bin/sh &
