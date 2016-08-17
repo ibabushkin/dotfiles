c3f4de77b7d39e555011bcc8064c4980230c1f11
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-08-17 14:49:48 +0200

    Added alarm notifications

---
+++ alarm.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+# read in commonly used notifications
+cat ~/dotfiles/alert_cache | sort | dmenu -p "time offset, message:" |
+tr , "\n" | ((read -A line1; sleep $line1) &&
+(read line2; notify-send -u critical -a alarm "$line2"))
+
