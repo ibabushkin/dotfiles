b66b9ba4575646df4cee4c4aedd69daaf972d4f2
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-08-26 18:56:18 +0200

    Fixed volume script to generate more compact output

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -5,4 +5,9 @@ else
     out=$(amixer -c 1 set Master $1 | grep "\[")
 fi
 
-echo vol: $(echo $out | cut -d ' ' -f 4,6) > ~/tmp/alsa_fifo
+if [[ "$out" =~ "[on]" ]]; then
+  echo "vol: $(echo $out | cut -d ' ' -f 4)" > ~/tmp/alsa_fifo
+else
+  echo "vol:%{F#ff0606} $(echo $out | cut -d ' ' -f 4)" > ~/tmp/alsa_fifo
+fi
+
