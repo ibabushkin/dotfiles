53144ee5ebd739e040b1fb99e327aba6dd06623c
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-08-27 21:21:04 +0200

    Removed unnecessary color change from battery script

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -2,7 +2,7 @@
 declare -i perc=$(cat /sys/class/power_supply/BAT0/capacity)
 if [[ perc < 15 ]]; then
   notify-send -u critical -a battery "low charge! plug the cable!"
-  echo "bat: %{F#ff0606}[$perc%]%{F#657b83}"
+  echo "bat: %{F#ff0606}[$perc%]"
 else
   echo "bat: [$perc%]"
 fi
