2b642bdb775f44702c327aa8371f0f2275dd3bf9
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-11-05 21:12:10 +0100

    Added pomodoro timer

---
+++ scripts/pom.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+pid=$(pidof spt)
+
+if [[ $1 == "-i" ]]; then
+  kill -s "USR1" $pid
+elif [[ $1 == "-p" ]]; then
+  kill -s "USR2" $pid
+  kill -s "USR1" $pid
+elif [[ $1 == "-t" ]]; then
+  if [[ $pid != "" ]]; then
+    kill -s "TERM" $pid
+  else
+    spt
+  fi
+fi
