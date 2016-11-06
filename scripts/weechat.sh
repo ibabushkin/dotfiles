7f74a0e2b40ad00dc06a1c9173c4b2c93e5e188f
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-11-06 11:40:32 +0100

    Fixed IRC setup

--- scripts/weechat.sh
+++ scripts/weechat.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-if $(tmux has-session -t weechat); then
-  tmux attach
+if [[ $1 != "--remote" ]]; then
+  if $(tmux has-session -t weechat); then
+    exec tmux attach
+  else
+    exec tmux new -s weechat weechat
+  fi
 else
-  tmux new -s weechat weechat
+  export TERM=xterm-256color
+  ssh -t toaster '~/weechat.sh'
 fi
