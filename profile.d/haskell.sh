a1d2d7525113ae35a0891143ecb8d9bd380610c3
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-11-13 17:37:32 +0100

    Added PATH update scripts for our locally installed Rust and Haskell software

---
+++ profile.d/haskell.sh
@@ -0,0 +1,4 @@
+[ -d ~/.cabal/bin ] && PATH=$PATH:~/.cabal/bin
+[ -d ~/.local/bin ] && PATH=$PATH:~/.local/bin
+
+export PATH
---
+++ profile.d/rust.sh
@@ -0,0 +1,5 @@
+[ -d ~/.multirust/toolchains/nightly/cargo/bin ] && PATH=$PATH:~/.multirust/toolchains/nightly/cargo/bin
+[ -d ~/.multirust/toolchains/beta/cargo/bin ] && PATH=$PATH:~/.multirust/toolchains/nightly/cargo/bin
+[ -d ~/.multirust/toolchains/stable/cargo/bin ] && PATH=$PATH:~/.multirust/toolchains/nightly/cargo/bin
+
+export PATH
