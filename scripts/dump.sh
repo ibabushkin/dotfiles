28fc490de87c6eca68905a7de2010a90d1a8c501
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-11-20 12:55:28 +0100

    Added file-dump-to-webserver script

---
+++ scripts/dump.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+remove=false
+
+if [[ $1 == "--help" ]]; then
+  exit 0
+elif [[ $1 == "-s" ]]; then
+  dir="img"
+  files="$2"
+  shift; shift
+  remove=true
+
+  scrot $@ $files
+else
+  dir=$1
+  shift
+  files=$@
+fi
+
+if $(scp -r $files "toaster:/var/www/htdocs/$dir"); then
+  echo ""
+
+  for file in $files; do
+    echo "https://twki.de/$dir/$(basename $file)"
+  done
+
+  if $remove; then
+    rm -r $files
+  fi
+fi
