2ef1986455ce4e0b145fdddba7e4747f80651f32
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-11-27 23:09:07 +0100

    Added fuzzy matching to bookmarks script

--- scripts/restore_bookmarks.sh
+++ scripts/restore_bookmarks.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
+fzy=false
+if [[ "$1" == "fzy" ]]; then
+	fzy=true
+	shift
+fi
+
 case "$1" in
-	firefox) program="firefox --new-tab" ;;
 	uzbl) program="uzbl-browser" ;;
 	*) program=$1 ;;
 esac
 shift
 
 sed -r "s/^# .*$//
-	s/^ *\* (.* :: )*//" $@ | while read line; do
-	case "$line" in
-		http*) $program $line 2>&1 > /dev/null & ;;
-	esac
-done
+	s/^ *\* (.* :: )*//" $@ | if [[ $fzy == true ]]; then
+	fzy | xargs $program &
+else
+	while read line; do
+		case "$line" in
+			http*) $program $line 2>&1 > /dev/null & ;;
+		esac
+	done
+fi
