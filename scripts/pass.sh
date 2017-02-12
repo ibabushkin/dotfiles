11e2d714a852c5121c7a220d4718cb81f20f3cb5
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-02-12 19:21:09 +0100

    Fixed gabelstaplerwm crashes and added notification to pass
    The crashes were due to xbacklight being not present on the system
    (needs proper fix in gabelstaplerwm - the helper function should spawn
    a new thread anyway, making a panic there nonfatal)
    
    The rest is purely cosmetical for people with fat fingers (that is, me).

--- scripts/pass.sh
+++ scripts/pass.sh
@@ -2,4 +2,5 @@
 # read in the password store and use dmenu to run pass on an entry from it
 find ~/.password-store -type f -name "*.gpg" |
 sed 's#.*\.password-store/##' | sed 's#\.gpg$##' |
-dmenu -n -i -y 20 | xargs pass -c
+dmenu -n -i -y 20 | xargs pass -c ||
+    notify-send -u critical -t 1000 -a pass "wrong passphrase"
