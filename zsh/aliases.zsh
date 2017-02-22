d802f4124f0cb4fc0d6e3c00ecc59353ee597a61
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-02-22 23:24:28 +0100

    Improved cmus integration a bit

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,16 +2,13 @@ alias rmdir='rmdir -v'
 alias ln='ln -v'
 alias chmod="chmod -c"
 alias chown="chown -c"
-alias sudo='sudo '
-alias pastebin='python2 ~/python/pastebin.py'
-alias emacs='echo "Haha, nice try!" && sleep .5 && vim $@'
 alias grep='grep --colour=auto'
 alias egrep='egrep --colour=auto'
 alias ls='ls --color=auto --human-readable --group-directories-first --classify'
-alias vim=nvim
 alias dump='~/dotfiles/scripts/dump.sh'
 alias control='~/dotfiles/scripts/control.sh'
 alias bookmarks='~/dotfiles/scripts/restore_bookmarks.sh'
+alias cmus='cmus && echo "" > ~/tmp/cmus_fifo'
 
 if command -v colordiff > /dev/null 2>&1; then
     alias diff="colordiff -Nuar"
