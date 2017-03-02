479195113c8d757071cc82b6be7047a8998dc613
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-03-02 16:58:16 +0100

    simplified weechat setup

--- scripts/weechat.sh
+++ scripts/weechat.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-if [ "$1" != "--remote" ]; then
-    if $(tmux has-session -t weechat); then
-        exec tmux attach
-    else
-        exec tmux new -s weechat weechat
-    fi
+if $(tmux has-session -t weechat); then
+    exec tmux attach
 else
-    export TERM=xterm-256color
-    ssh -t toaster '~/weechat.sh'
+    exec tmux new -s weechat weechat
 fi
