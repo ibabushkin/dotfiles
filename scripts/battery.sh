24afcf4a42e1723aac8c99de625939a874129550
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-03-08 11:19:31 +0100

    Improved battery status indicator

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 perc=$(($(cat /sys/class/power_supply/BAT0/capacity)))
-if [ $perc -lt 20 ]; then
+
+case $(cat /sys/class/power_supply/BAT0/status) in
+    Charging)
+        status="+"
+        ;;
+    Unknown)
+        status="±"
+        ;;
+    Discharging)
+        status="-"
+        ;;
+    Full)
+        status="●"
+        ;;
+    *)
+        status="e"
+esac
+
+if [ $perc -lt 20 -a $status != "+" ]; then
     notify-send -u critical -a battery "low charge! plug the cable!"
-    echo "bat: %{F#ff0606}[$perc%]"
+    echo "bat: %{F#ff0606}$status[$perc%]"
 elif [ $perc -lt 50 ]; then
-    echo "bat: %{F#dddd06}[$perc%]"
+    echo "bat: %{F#dddd06}$status[$perc%]"
 else
-    echo "bat: %{F#268bd2}[$perc%]"
+    echo "bat: %{F#268bd2}$status[$perc%]"
 fi
