4b29627a6c1f5c3d796081d6ce072e9c36dacc7c
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-03-13 12:53:38 +0100

    Updated gabelstaplerwm config and improved workspace script

--- scripts/workspace.sh
+++ scripts/workspace.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-sed 's/,.*//' ~/dotfiles/workspace_cache | sort |
-dmenu -n -i -y 20 | xargs -I{} grep {}, ~/dotfiles/workspace_cache |
-sed 's/.*,/exec st tmux new -c /' | /bin/sh &
+line=$(sed 's/,.*//' ~/dotfiles/workspace_cache | sort |
+dmenu -n -i -y 20 | xargs -I{} grep {}, ~/dotfiles/workspace_cache)
+workspace=$(echo $line | sed 's/,.*//')
+path=$(echo $line | sed 's/.*,//')
+
+st tmux new -s "${workspace}-0" -c $path &
+st tmux new -s "${workspace}-1" -c $path &
+exec st tmux new -s "${workspace}-2" -c $path
