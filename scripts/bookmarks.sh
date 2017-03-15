3210a63eaf9c2f3db0d22f88b7d23ed9424b95ca
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-03-15 11:26:22 +0100

    Some renaming of already present scripts

---
+++ scripts/bookmarks.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+fzy=false
+if [ "$1" = "fzy" ]; then
+    fzy=true
+    shift
+fi
+
+case "$1" in
+    uzbl) program="uzbl-browser" ;;
+    *) program=$1 ;;
+esac
+shift
+
+if $fzy; then
+    cat $@ | fzy | sed -r "s/^# .*$//
+    s/^ *\* (.* :: )*//"
+else
+    sed -r "s/^# .*$//
+    s/^ *\* (.* :: )*//" $@
+fi |
+while read line; do
+    case "$line" in
+        http*) $program $line 2>&1 > /dev/null & ;;
+    esac
+done
