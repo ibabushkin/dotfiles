bf1314cd11bb5011e437c11ab231412455c2125e
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-03-16 00:27:09 +0100

    Ported alarm.zsh to sh

--- scripts/alarm.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-# read in commonly used notifications
-sort ~/dotfiles/alarm_cache | dmenu -y 20 -p "time offset, message:" |
-tr , "\n" | ((read -A line1; sleep $line1) &&
-(read line2; notify-send -u critical -a alarm "$line2"))
