a510f944386b6d65a7233408323fb4282fa62271
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-03-15 11:25:56 +0100

    Added reattach script for tmux

---
+++ scripts/reattach.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# read in the currently active tmux sessions
+session=$(tmux list-sessions | dmenu -y 20 -l 20 | cut -d : -f 1)
+
+[ -z $session ] && exit 0
+
+st tmux attach -t $session 2> /dev/null
