9e3d9d9d57650c0e06986ec6cd5062b082cf42ca
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2017-03-16 00:44:03 +0100

    Ported network scripts from zsh to sh

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,11 +1,11 @@
-#!/bin/zsh
+#!/bin/sh
 out=$(nmcli -m tabular -t -f "IP4.ADDRESS" dev show enp0s25)
-if [[ "$out" != "" ]]; then
+if [ "$out" != "" ]; then
     echo "eth: %{F#268bd2}[$out]"
 else
     out=$(nmcli -m tabular -t -f "GENERAL.CONNECTION,IP4.ADDRESS" dev show wlp2s0 |
     sed -z "s/\\n/: %{F#268bd2}[/")
-    if [[ "$out" =~ "--" ]]; then
+    if echo "$out" | grep "--"; then
         echo "no net"
     else
         echo "$out]"
