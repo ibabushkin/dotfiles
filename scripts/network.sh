482f68c9d096713e43e313660d765954f05dd644
Inokentiy Babushkin <twk@twki.de>
2017-03-29 16:57:49 +0200

    Added vpn status indicator to network scripts

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+if ip route show to 0/0 | grep "personal" > /dev/null; then
+    vpn="(vpn)"
+fi
+
 out=$(nmcli -m tabular -t -f "IP4.ADDRESS" dev show enp0s25)
 if [ "$out" != "" ]; then
-    echo "enp0s25: %{F#268bd2}[$out]"
+    echo "enp0s25$vpn: %{F#268bd2}[$out]"
 else
     out=$(nmcli -m tabular -t -f "GENERAL.CONNECTION,IP4.ADDRESS" dev show wlp2s0 |
-    sed -z "s/\\n/: %{F#268bd2}[/")
+    sed -z "s/\\n/$vpn: %{F#268bd2}[/")
     if echo "$out" | grep -e "--" > /dev/null; then
         echo "no net"
     else
