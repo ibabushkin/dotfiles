a050ef6b6f9a867f4acaef560766f14b029f47ef
Inokentiy Babushkin <twk@twki.de>
2017-03-29 10:40:47 +0200

    Updated network scripts (cosmetics)

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 out=$(nmcli -m tabular -t -f "IP4.ADDRESS" dev show enp0s25)
 if [ "$out" != "" ]; then
-    echo "eth: %{F#268bd2}[$out]"
+    echo "enp0s25: %{F#268bd2}[$out]"
 else
     out=$(nmcli -m tabular -t -f "GENERAL.CONNECTION,IP4.ADDRESS" dev show wlp2s0 |
     sed -z "s/\\n/: %{F#268bd2}[/")
