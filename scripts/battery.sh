f069b18d5b2cc6d0e6e61179622295d497c6de48
Inokentiy Babushkin <twk@twki.de>
2017-04-12 13:14:54 +0200

    Made the battery script work with the secondary battery as well

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,28 +1,48 @@
 #!/bin/sh
-perc=$(($(cat /sys/class/power_supply/BAT0/capacity)))
+perc1=$(($(cat /sys/class/power_supply/BAT0/capacity)))
 
 case $(cat /sys/class/power_supply/BAT0/status) in
     Charging)
-        status="+"
+        status1="+"
         ;;
     Unknown)
-        status="±"
+        status1="±"
         ;;
     Discharging)
-        status="-"
+        status1="-"
         ;;
     Full)
-        status=""
+        status1=""
         ;;
     *)
-        status="e"
+        status1="e"
 esac
 
-if [ $perc -lt 20 ] && [ $status != "+" ]; then
-    notify-send -u critical -a battery "low charge! plug the cable!"
-    echo "bat: %{F#ff0606}$status[$perc%]"
-elif [ $perc -lt 50 ]; then
-    echo "bat: %{F#dddd06}$status[$perc%]"
+if [ -d /sys/class/power_supply/BAT1 ]; then
+    case $(cat /sys/class/power_supply/BAT0/status) in
+        Charging)
+            status2="+"
+            ;;
+        Unknown)
+            status2="±"
+            ;;
+        Discharging)
+            status2="-"
+            ;;
+        Full)
+            status2=""
+            ;;
+        *)
+            status2="e"
+    esac
+    bat2=" / $status2[$(cat /sys/class/power_supply/BAT1/capacity)%]"
+fi
+
+if [ $perc1 -lt 20 ] && [ $status1 != "+" ]; then
+    notify-send -u critical -a battery "low charge! plug the cable or swap battery!"
+    echo "bat: %{F#ff0606}$status1[$perc1%]$bat2"
+elif [ $perc1 -lt 50 ]; then
+    echo "bat: %{F#dddd06}$status1[$perc1%]$bat2"
 else
-    echo "bat: %{F#268bd2}$status[$perc%]"
+    echo "bat: %{F#268bd2}$status1[$perc1%]$bat2"
 fi
