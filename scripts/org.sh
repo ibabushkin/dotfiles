0073b5273ec9b3f09c0b41eb3e07deeb33f63e6f
Inokentiy Babushkin <twk@twki.de>
2017-04-13 14:13:45 +0200

    partially reverted some changes to the way we call morgue

--- scripts/org.sh
+++ scripts/org.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-notify-send -t 0 -u low -a morgue "$(morgue -d -f pango -t slow -i -m timed ~/org/notes.md ~/org/uni.md)"
+notify-send -t 0 -u low -a morgue "$(morgue -d -f pango -t slow -i ~/org/notes.md ~/org/uni.md)"
