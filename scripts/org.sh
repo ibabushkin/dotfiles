fe50c9599deb073fb13c7e32c3ca1ab12e504705
Inokentiy Babushkin <twk@twki.de>
2017-04-25 09:10:04 +0200

    More flexible agenda generator script

--- scripts/org.sh
+++ scripts/org.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-notify-send -t 0 -u low -a morgue "$(morgue -f pango -t slow -i ~/org/notes.md ~/org/uni.md)"
+if [ "$1" = "full" ]; then
+    mode="both"
+else
+    mode="timed"
+fi
+
+files="$HOME/org/notes.md $HOME/org/uni.md"
+
+notify-send -t 0 -u low -a morgue "$(morgue -m $mode -f pango -t slow -i $files)"
