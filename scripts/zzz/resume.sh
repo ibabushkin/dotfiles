7b0bb308cfe8cbe7fde1aeef91e258f957d543d5
Inokentiy Babushkin <twk@twki.de>
2017-04-28 22:38:39 +0200

    Better backlight handling

---
+++ scripts/zzz/resume.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+intel_reg write 0x00061254 "$(cat /home/twk/tmp/backlight)" 2> /dev/null
--- scripts/zzz/suspend.sh
+++ scripts/zzz/suspend.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 doas -u twk gpgconf --kill gpg-agent
 echo "" > /home/twk/tmp/gpg_status_fifo
+intel_reg read 0x00061254 2> /dev/null | sed "s/.*: //" > /home/twk/tmp/backlight
 DISPLAY=:0 doas -u twk slock &
 sleep .2
+
