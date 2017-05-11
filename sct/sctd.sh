73c41a4c6c1c25c04df0720f64aae9da75cfe650
Inokentiy Babushkin <twk@twki.de>
2017-05-11 11:59:19 +0200

    Added screen temperature daemon

---
+++ sct/sctd.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Copyright (c) 2017 Aaron Bieber <aaron@bolddaemon.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+S=4500
+INC=2
+SCT=$(which sct)
+
+if [ ! -e "$SCT" ]; then
+    echo "Please install sct!"
+    exit 1;
+fi
+
+setHM() {
+    H=$(date +"%H" | sed -e 's/^0//')
+    M=$(date +"%M" | sed -e 's/^0//')
+    HM=$((H*60 + M))
+}
+
+setHM
+
+if [ $HM -gt 720 ]; then # t > 12:00
+    for _ in $(jot $((1440 - HM)));  do
+        S=$((S+INC))
+    done
+else # t <= 12:00
+    for _ in $(jot $HM); do
+        S=$((S+INC))
+    done
+fi
+
+while true; do
+    setHM
+
+    if [ $HM -gt 720 ]; then
+        S=$((S-INC))
+    else
+        S=$((S+INC))
+    fi
+
+    $SCT $S
+
+    sleep 60
+done
