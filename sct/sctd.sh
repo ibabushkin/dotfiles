c03882f1425a945e05dc492800c7e599712784d5
Inokentiy Babushkin <twk@twki.de>
2017-05-12 20:09:55 +0200

    Fixed a dumb typo

--- sct/sctd.sh
+++ sct/sctd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Copyright (c) 2017 Aaron Bieber <aaron@bolddaemon.com>
-# Copryright (c) 2017 Inokentiy Babushkin <twk@twki,de>
+# Copryright (c) 2017 Inokentiy Babushkin <twk@twki.de>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
