025703e0ae90aa1e4e283cf217c5b222498c1ad1
Inokentiy Babushkin <twk@twki.de>
2017-05-14 11:34:24 +0200

    Fixed color temperature reset on resume

--- scripts/zzz/resume.sh
+++ scripts/zzz/resume.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 intel_reg write 0x00061254 "$(cat /home/twk/tmp/backlight)" 2> /dev/null
-DISPLAY=:1.0 /home/twk/dotfiles/sct/sctd.sh oneshot
+DISPLAY=:0 /home/twk/dotfiles/sct/sctd.sh oneshot
