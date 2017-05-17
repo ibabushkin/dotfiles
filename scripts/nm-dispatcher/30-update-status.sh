1ae75bc0adf6aae630a985f0e307b9fbbe3d2d0c
Inokentiy Babushkin <twk@twki.de>
2017-05-17 20:45:05 +0200

    Some work on the clubstatus script

--- scripts/nm-dispatcher/30-update-status.sh
+++ scripts/nm-dispatcher/30-update-status.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 sleep .2
-exec "$DOTFILES"/scripts/network.sh > "$MYHOME"/tmp/net_fifo
+"$DOTFILES"/scripts/network.sh > "$MYHOME"/tmp/net_fifo
+
+if [ "$2" = "up" ]; then
+    "$DOTFILES"/scripts/clubstatus.sh > /dev/null &
+    disown
+elif [ "$2" = "down" ]; then
+    pkill clubstatus.sh
+fi
