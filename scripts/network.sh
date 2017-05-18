497f0e1cd3f776bcfadf448fa19c4e61a13e8134
Inokentiy Babushkin <twk@twki.de>
2017-05-18 17:19:28 +0200

    Updated network script to work with newest NetworkManager

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if ip route show to 0/0 | grep "personal" > /dev/null; then
+if ip route show to 0/0 | grep -q "personal"; then
     vpn="(vpn)"
 fi
 
@@ -7,11 +7,10 @@ out=$(nmcli -m tabular -t -f "IP4.ADDRESS" dev show enp0s25)
 if [ "$out" != "" ]; then
     echo "enp0s25$vpn: %{F#6684e1}[$out]"
 else
-    out=$(nmcli -m tabular -t -f "GENERAL.CONNECTION,IP4.ADDRESS" dev show wlp2s0 |
-    sed -z "s/\\n/$vpn: %{F#6684e1}[/")
-    if echo "$out" | grep -e "--" > /dev/null; then
-        echo "no net"
+    out=$(nmcli -m tabular -t -f "GENERAL.CONNECTION,IP4.ADDRESS" dev show wlp2s0)
+    if [ "$out" != "" ]; then
+        echo "$out]"| sed -z "s/\\n/$vpn: %{F#6684e1}[/"
     else
-        echo "$out]"
+        echo "no net"
     fi
 fi
