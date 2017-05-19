5cc8279d5858d1da7911587b372fbf0698877f13
Inokentiy Babushkin <twk@twki.de>
2017-05-19 19:34:41 +0200

    Magic! (also, our club status script now actually works)

--- scripts/nm-dispatcher/30-update-status.sh
+++ scripts/nm-dispatcher/30-update-status.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 sleep .2
-"$DOTFILES"/scripts/network.sh > "$MYHOME"/tmp/net_fifo
 
 if [ "$2" = "up" ]; then
-    "$DOTFILES"/scripts/clubstatus.sh > /dev/null &
-    disown
+    nohup "$DOTFILES"/scripts/clubstatus.sh < /dev/null > /dev/null 2>&1 &
 elif [ "$2" = "down" ]; then
     pkill clubstatus.sh
 fi
