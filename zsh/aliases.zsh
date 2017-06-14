6229c33473f3abc086cd332c6bb4ef72d636b38e
Inokentiy Babushkin <twk@twki.de>
2017-06-14 16:58:18 +0200

    Added an alias for backlight fic

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,15 +3,18 @@ alias rmdir='rmdir -v'
 alias ln='ln -v'
 alias chmod="chmod -c"
 alias chown="chown -c"
+alias ls='ls --color=auto --human-readable --group-directories-first --classify'
 alias grep='grep --colour=auto'
 alias egrep='egrep --colour=auto'
-alias ls='ls --color=auto --human-readable --group-directories-first --classify'
-alias dump='~/dotfiles/scripts/dump.sh'
-alias control='~/dotfiles/scripts/control.sh'
-alias bookmarks='~/dotfiles/scripts/bookmarks.sh'
-alias reattach='~/dotfiles/scripts/reattach.sh'
+
 alias cmus='cmus && echo "" > ~/tmp/cmus_fifo'
 alias killgpg='gpgconf --kill gpg-agent && echo "" > ~/tmp/gpg_status_fifo'
+
+alias backlight='~/dotfiles/scripts/backlight_fix.sh'
+alias bookmarks='~/dotfiles/scripts/bookmarks.sh'
+alias control='~/dotfiles/scripts/control.sh'
+alias dump='~/dotfiles/scripts/dump.sh'
+alias reattach='~/dotfiles/scripts/reattach.sh'
 
 if command -v colordiff > /dev/null 2>&1; then
     alias diff="colordiff -Nuar"
