064cd0865f7cefa2273e57c0f0c49f99a9a5c9b2
Inokentiy Babushkin <twk@twki.de>
2020-09-17 10:48:31 +0200

    profiles: update paths.

--- profile.d/haskell.sh
+++ profile.d/haskell.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 [ -d ~/.cabal/bin ] && PATH=$PATH:~/.cabal/bin
-[ -d ~/.local/bin ] && PATH=$PATH:~/.local/bin
 
 export PATH
--- profile.d/texlive.sh
+++ profile.d/texlive.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-[ -d /opt/texlive/2016/bin/x86_64-linux ] && PATH=$PATH:/opt/texlive/2016/bin/x86_64-linux
+[ -d /opt/texlive/2019/bin/x86_64-linux ] && PATH=$PATH:/opt/texlive/2019/bin/x86_64-linux
 
 export PATH
