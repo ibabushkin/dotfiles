8e9e9fe5134470681e4babe143e29e7e2f4287d9
Inokentiy Babushkin <twk@twki.de>
2020-09-20 13:51:09 +0200

    switch from dmenu to rofi.

--- scripts/pass.sh
+++ scripts/pass.sh
@@ -3,9 +3,9 @@ if [ "$1" = "dummy" ]; then
     # access a dummy entry to load the key
     pass dummy > /dev/null 2>&1 && echo "+" > ~/tmp/gpg_status_fifo
 else
-    # read in the password store and use dmenu to run pass on an entry from it
+    # read in the password store and use rofi to run pass on an entry from it
     find ~/.password-store -type f -name "*.gpg" |
     sed 's#.*\.password-store/##' | sed 's#\.gpg$##' |
-    dmenu -n -i -y 20 | (xargs pass -c && echo "+" > ~/tmp/gpg_status_fifo) ||
+    rofi -dmenu -n -i | (xargs pass -c && echo "+" > ~/tmp/gpg_status_fifo) ||
         notify-send -u critical -t 1000 -a pass "wrong passphrase"
 fi
